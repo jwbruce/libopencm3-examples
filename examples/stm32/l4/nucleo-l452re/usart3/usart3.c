7672a1dbe8b9f7dbfd3d634a42523e28a1886b74
kester <kesternucum@yahoo.com>
2022-04-27 14:18:21 -0500

    Removed STM32L4 defines from USART3 example

--- examples/stm32/l4/nucleo-l452re/usart3/usart3.c
+++ examples/stm32/l4/nucleo-l452re/usart3/usart3.c
@@ -18,9 +18,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
- #ifndef STM32L4
-    #define STM32L4
- #endif
 
 #include <stdbool.h>
 #include <stdint.h>
